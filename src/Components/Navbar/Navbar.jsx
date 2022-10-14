06235ec91f42f49b55dc914ec02345e3210d33f1
chaeBrian <bmichelibanez@gmail.com>
2022-10-13 21:33:19 -0300

    link x navlink

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import CartWidget from './CartWidget/CartWidget';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 //Stylesheet
 import './navbar.css';
 
 const Navbar = () => {
   return (
     <nav className='nav'>
-        <Link className='nav__link--modified' to='/'>f w c</Link>
+        <NavLink className='nav__link--modified' to='/'>f w c</NavLink>
         <ul className='nav__ul'>
             <li>
-              <Link className='nav__link' to='/category/home-shirts'>
+              <NavLink className='nav__link' to='/category/home-shirts'>
                 home
-                </Link>
+                </NavLink>
               </li>
             <li>
-              <Link className='nav__link' to='/category/away-shirts'>
+              <NavLink className='nav__link' to='/category/away-shirts'>
                 away
-                </Link>
+                </NavLink>
               </li>
             <li>
-              <Link className='nav__link' to='/category/goalkeeper-shirts'>
+              <NavLink className='nav__link' to='/category/goalkeeper-shirts'>
                 goalkeeper
-                </Link>
+                </NavLink>
               </li>
             <li>
-              <Link className='nav__link' to='/cart'>
+              <NavLink className='nav__link' to='/cart'>
                 <CartWidget />
-                </Link>
+                </NavLink>
               </li>
         </ul>
     </nav>
